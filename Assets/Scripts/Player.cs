2229eb5c593609613b6ce712891c538311261ecd
Eugeniov <bastien.coquelin@gmail.com>
2021-01-15 11:18:48 +0100

    push player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Vector2 speed = new Vector2(0.5f, 0.5f);
+    private Vector2 bulletspeed;
+    public int healthPts;
+    public bool death;
+    public double fireRate;
+
+    [SerializeField]
+    private Rigidbody2D rigidbody;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,26 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButton("Fire1"))
+        {
+            Shoot();
+        }
+
+        /*if (healthPts <= 0 && !death)
+        {
+            death = true;
+            Destroy(gameObject);
+            GameplayManager.Instance.ShowGameOver();
+        }*/
+    }
+
+    void Shoot()// commande le tir 
+    {
+        if (Time.time > time)
+        {
+            GameObject spawnedbullet = Instantiate(spawnedbullet, transform.position + bulletOffset, transform.rotation);//instancie la bullet
+            spawnedbullet.GetComponent<Rigibody2D>().velocity = bulletspeed;//lui donne sa vitesse
+            time = Time.time + fireRate;// lui limite sa cadence de tir
+        }
     }
 }
