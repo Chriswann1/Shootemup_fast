a2b31731ad4fc7d686fc9f3feb944ac2d4582ee6
Eugeniov <bastien.coquelin@gmail.com>
2021-01-22 11:50:29 +0100

    add healthpt getsion

---
+++ Assets/Scripts/scriptEnnemy1.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+
+public class scriptEnnemy1 : MonoBehaviour
+{
+    public int healthPts;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (healthPts <= 0)
+        {
+            GameplayManager.Instance.Score += 100;
+            Destroy(gameObject);
+        }
+        if (other.gameObject.tag == "bullet")
+        {
+            healthPts--;
+            Destroy(other.gameObject);
+        }
+
+    }
+}
